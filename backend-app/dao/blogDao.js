1cf7e9de1c36f4522073838c7ca8435edad8a9e6
moheqionglin <zhouwanli2009@yeah.net>
2021-03-11 10:38:04 +0800

    Update blogDao.js
    change blog list page content

--- backend-app/dao/blogDao.js
+++ backend-app/dao/blogDao.js
@@ -16,7 +16,7 @@ var getBlockListByPage = async(currentPage) =>{
         attributes: ['id', 'title', 'introduction', 'image', 'readCount', 'collectionType', 'keyWord', 'createdAt' ],
         order:'readCount desc, id desc',
         where:{
-            collectionType: {$in: collectionType.BLOG, collectionType.BOOK, collectionType.SERIALIZE}
+            collectionType: {$in: [collectionType.BLOG, collectionType.BOOK, collectionType.SERIALIZE]}
         },
         limit: pagiationConf.PAGE_SIZE,
         offset: (currentPage - 1) * pagiationConf.PAGE_SIZE
